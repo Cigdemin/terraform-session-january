60a609942685612108be3cde999ce24d98b758b0
cigdemin <cigmenci@hotmail.com>
2023-02-06 14:50:35 -0600

    fixed my code

--- session-2/tasks/security-group/main.tf
+++ session-2/tasks/security-group/main.tf
@@ -11,20 +11,48 @@ resource "aws_security_group" "main_sg" {
   description = "This security group allows inbound traffic for ports: 80,443,22,3306"
   vpc_id = vpc-08d1b3e58f4fb4b4b
 
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = [ "80", "443", "22", "3306" ]
-    to_port          = [ "80" , "443" , "22" , "3306" ]
+  ingress = [
+    {
+    description      = "http from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }, 
+  {
+    description      = "https from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  },
+  {
+    description      = "ssh from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  },
+  {
+    description      = "sql from VPC"
+    from_port        = 3306
+    to_port          = 3306
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+  ]
 
-  egress {
+  egress = [
+  {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+  ]
 }
