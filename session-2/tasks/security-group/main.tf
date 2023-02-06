a48132944f2e868dbb473145d0d52a2f1baf8dc5
cigdemin <cigmenci@hotmail.com>
2023-02-06 14:37:31 -0600

    added main-.tf file for security group

---
+++ session-2/tasks/security-group/main.tf
@@ -0,0 +1,30 @@
+# OpenEC2 console and chose Security group from left side
+# Create new security group
+# give a security gorup name
+# give a description " This security group allows inbound traffic for ports: 80,443,22,3306 "
+# chose a VPC "our default VPC ID: vpc-08d1b3e58f4fb4b4b "
+# add inbound rules
+   # Type: give protocol, protocol range , source and CIDR block
+   # outbound rules is default open to all trafic
+resource "aws_security_group" "main_sg" {
+  name        = "allow_inbound_traffic"
+  description = "This security group allows inbound traffic for ports: 80,443,22,3306"
+  vpc_id = vpc-08d1b3e58f4fb4b4b
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = [ "80", "443", "22", "3306" ]
+    to_port          = [ "80" , "443" , "22" , "3306" ]
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
