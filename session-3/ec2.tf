4cb4c702d6692cafc2e75b0532acd8ef138fffa3
cigdemin <cigmenci@hotmail.com>
2023-02-06 20:15:03 -0600

    dynamic referances code fixed

--- session-3/ec2.tf
+++ session-3/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "first_ec2" {
   ami           = "ami-06e85d4c3149db26a"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [ "aws_security_group.main_sg.id" ] # alist of strings
+  vpc_security_group_ids = [ aws_security_group.main_sg.id ] # alist of strings #dynamic referances
 }
 
 #List?
@@ -21,3 +21,6 @@ resource "aws_instance" "first_ec2" {
 #reference to resource : to dynamicly referance security group id without creating it. IT will be created by the same thime or terraform go and find for me
 # first_label.second_label.attribute
 #attribute that you can know after creation only for example id of security group it means you can know after you create it
+
+# "aws_security_group.main_sg.id" = static or hard coded value(string)
+# aws_security_group.main_sg.id = reference to resource >> (string,number,boolean)
