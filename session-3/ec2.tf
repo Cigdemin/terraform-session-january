e5a9c6fb95188d5910d0060ec0660296191b0867
cigdemin <cigmenci@hotmail.com>
2023-02-06 20:36:32 -0600

    tags added

--- session-3/ec2.tf
+++ session-3/ec2.tf
@@ -2,6 +2,9 @@ resource "aws_instance" "first_ec2" {
   ami           = "ami-06e85d4c3149db26a"
   instance_type = "t2.micro"
   vpc_security_group_ids = [ aws_security_group.main_sg.id ] # alist of strings #dynamic referances
+  tags = {
+    Name = "developer"
+  }
 }
 
 #List?
