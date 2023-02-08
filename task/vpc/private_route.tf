21a72c4d947b10ac65d0b058bb37bb53aaed8915
cigdemin <cigmenci@hotmail.com>
2023-02-08 10:51:39 -0600

    added elastic ip and allocate with pri network

--- task/vpc/private_route.tf
+++ task/vpc/private_route.tf
@@ -10,10 +10,16 @@
 # edit route and add route
 # choose destination :0.0.0.0/0 choose NatGW that created and save changes
 
-resource "aws_nat_gateway" "task_natgw" {
-  connectivity_type = "private"
-  subnet_id         = aws_subnet.public_subnet_a.id
+resource "aws_eip" "task-eip" {
 }
+
+resource "aws_nat_gateway" "task_natgw" {
+  allocation_id = aws_eip.task-eip.id
+  connectivity_type = "public"
+  subnet_id         = aws_subnet.public_subnet_a.id
+  depends_on = [aws_internet_gateway.task_igw]
+}
+
 
 resource "aws_route_table" "tasktest_private_route_table" {
   vpc_id = aws_vpc.my_task_vpc.id
