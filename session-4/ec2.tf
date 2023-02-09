38712042ed34488df12e046a08888b785c8fc4eb
cigdemin <cigmenci@hotmail.com>
2023-02-08 20:53:33 -0600

    new ec2 added

--- session-4/ec2.tf
+++ session-4/ec2.tf
@@ -3,8 +3,19 @@ resource "aws_instance" "first_ec2" {
   instance_type = var.instance_type
   vpc_security_group_ids = [ aws_security_group.main_sg.id ] # alist of strings #dynamic referances
   tags = {
-    Name = var.env
+    Name = var.env-instances
   }
 }
+
+resource "aws_instance" "first_ec2" {
+  ami           = data.aws_ami.amazon_linux_2.id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [ aws_security_group.main_sg.id ] # alist of strings #dynamic referances
+  tags = {
+    Name = var.env-frontend-instance
+  }
+}
+
+
 
 #interpolation : outsource of variable
