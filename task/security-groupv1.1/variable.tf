d013cf7bc57b621985328490b9583bbb706bd813
cigdemin <cigmenci@hotmail.com>
2023-02-09 13:36:23 -0600

    fixed main.tf

--- task/security-groupv1.1/variable.tf
+++ task/security-groupv1.1/variable.tf
@@ -12,14 +12,65 @@ variable "vpc_id" {
   default = "vpc-08d1b3e58f4fb4b4b"
 }
 
+variable "cidr_blocks" {
+  type = string
+  description = "defines CIDR block"
+  default = "0.0.0.0/0"
+}
 
 variable "port1" {
-  type = list
+  type = string
   description = "giving port numbers"
-  default = [ 22, 80, 443, 3306]
+  default = "22"
+}
+
+variable "port2" {
+  type = string
+  description = "giving port numbers"
+  default = "80"
+}
+
+variable "port3" {
+  type = string
+  description = "giving port numbers"
+  default = "443"
+}
+variable "port5" {
+  type = string
+  description = "giving port numbers"
+  default = "3306"
+}
+variable "port6" {
+  type = string
+  description = "giving port numbers"
+  default = "3389" 
+}
+
+variable "port7" {
+  type = string
+  description = "giving port numbers"
+  default = "5432" 
+}
+
+variable "port8" {
+  type = string
+  description = "giving port numbers"
+  default = "888" 
 }
 
 
+variable "port9" {
+  type = string
+  description = "giving port numbers"
+  default = "999" 
+}
+
+
+variable "port10" {
+  type = string
+  description = "giving port numbers"
+  default = "1000" 
+}
 
 variable "protocol" {
   type = string
