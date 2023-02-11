5de37741665cf7f8a8259dcc27641a10c2533261
cigdemin <cigmenci@hotmail.com>
2023-02-11 12:39:45 -0600

    sg updated

--- session-5/sg.tf
+++ session-5/sg.tf
@@ -1,17 +1,21 @@
 resource "aws_security_group" "main_sg" {   
   name        = "main"
   description = "Allow SSH inbound traffic"
+}
 
-  ingress {
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"  # string
-    cidr_blocks      = ["0.0.0.0/0"] # list of strings
-  }
-   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1" # string # All ports and protocols
-    cidr_blocks      = ["0.0.0.0/0"] 
-  }
+resource "aws_security_group_rule" "ingress" {
+  type              = "ingress"
+  to_port           = element(var.ports , 0) #22
+  from_port         = element(var.ports , 0) #22
+  cidr_blocks = [ "0.0.0.0/0" ]
+  security_group_id = aws_security_group.main_sg.id
+  protocol          = "tcp"
+}
+resource "aws_security_group_rule" "egress" {
+  type              = "egress"
+  to_port           = 0
+  protocol          = "-1"
+  from_port         = 0
+  security_group_id = aws_security_group.main_sg.id
+  cidr_blocks = [ "0.0.0.0/0" ]
 }
