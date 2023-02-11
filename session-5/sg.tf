7459890474f6ad641c2e241cbaad3142d8de0ad8
cigdemin <cigmenci@hotmail.com>
2023-02-11 13:07:54 -0600

    updated "count.index"

--- session-5/sg.tf
+++ session-5/sg.tf
@@ -6,8 +6,8 @@ resource "aws_security_group" "main_sg" {
 resource "aws_security_group_rule" "ingress" {
   count = 2
   type              = "ingress"
-  to_port           = element(var.ports , 0) #22
-  from_port         = element(var.ports , 0) #22
+  to_port           = element(var.ports , count.index) #22
+  from_port         = element(var.ports , count.index) #22
   cidr_blocks = [ "0.0.0.0/0" ]
   security_group_id = aws_security_group.main_sg.id
   protocol          = "tcp"
@@ -20,3 +20,10 @@ resource "aws_security_group_rule" "egress" {
   security_group_id = aws_security_group.main_sg.id
   cidr_blocks = [ "0.0.0.0/0" ]
 }
+
+# count.index = count the indexes
+
+#count=2
+#count.index = 0,1
+#count=5
+#count.index=0,1,2,3,4
