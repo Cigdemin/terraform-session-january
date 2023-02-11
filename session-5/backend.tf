c21d35af77d2f1aa6fbbebbd33f121c4583ec100
cigdemin <cigmenci@hotmail.com>
2023-02-11 11:35:38 -0600

    dynamodb table added

--- session-5/backend.tf
+++ session-5/backend.tf
@@ -3,5 +3,12 @@ terraform {
     bucket = "terraform-session-september-backend-cigdem"
     region = "us-west-2"
     key    = "session-5/terraform.tfstate"  #where does terraform need to store your file.Path or 
+    dynamodb_table = "terraform-session-sep-state-lock" #Lock table
   }
 }
+
+#Most important rule!
+#you can not use any variables in key!
+#
+
+
