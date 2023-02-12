c22fa6bd9963295974d3096bb702ec94634e3125
cigdemin <cigmenci@hotmail.com>
2023-02-12 15:39:11 -0600

    code fixed

--- task/security-groupv1.2/variablev1.2.tf
+++ task/security-groupv1.2/variablev1.2.tf
@@ -3,21 +3,8 @@
 variable "sg_name" {
   type = string
   description = "This is asecurity group opens 10 ports"
-  default = "task_sg"
+  default = "tasksg"
 }
-
-variable "ports" {
-  type = list(string)
-  description = "list of ports"
-  default = ["22","80","3306","443","23","110","995","143","993","21"]
-}
-
-variable "vpc_id" {
-  type = string
-  description = "to give vpc id"
-  default = "vpc-08d1b3e58f4fb4b4b" # default VPC Id
-}
-
 variable "cidr_all {
   type = string
   description = "defines CIDR block for all traffic"
@@ -26,5 +13,15 @@ variable "cidr_all {
 variable "protocol" {
   type = string
   description = "specify protocol type"
-  default ="tcp"
+  default = "tcp"
+}
+variable "ports" {
+  type = list(string)
+  description = "list of ports"
+  default = ["22","80","3306","443","23","110","995","143","993","21"]
+}
+variable "vpc_id" {
+  type = string
+  description = "identifiying default vpc id"
+  default = "vpc-08d1b3e58f4fb4b4b"
 }
