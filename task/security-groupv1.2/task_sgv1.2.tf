39220e5409f029da38930621b0f1df10cce73bd6
cigdemin <cigmenci@hotmail.com>
2023-02-12 14:34:39 -0600

    security group v1.2 added

---
+++ task/security-groupv1.2/task_sgv1.2.tf
@@ -0,0 +1,24 @@
+   
+resource "aws_security_group" "task_sg" {
+  name        = var.sg_name
+  description = "This security group allows inbound traffic for 10 ports"
+  vpc_id = var.vpc_id
+}
+resource "aws_security_group_rule" "ingress" {
+  count = length(var.ports)
+  type              = "ingress"
+  to_port           = element(var.ports , count.index) 
+  from_port         = element(var.ports , count.index)
+  cidr_blocks = [ var.cidr_all]
+  security_group_id = aws_security_group.task_sg.id
+  protocol          = var.protocol
+}
+
+resource "aws_security_group_rule" "egress" {
+  type              = "egress"
+  to_port           = 0
+  protocol          = "-1"
+  from_port         = 0
+  security_group_id = aws_security_group.task_sg.id
+  cidr_blocks = [ var.cidr_all ]
+}
