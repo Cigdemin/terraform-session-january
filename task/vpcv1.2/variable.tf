9a7d2c6a997b6c087b2c4cb9671b3dce53789f6e
cigdemin <cigmenci@hotmail.com>
2023-02-13 15:23:37 -0600

    code fixed

--- task/vpcv1.2/variable.tf
+++ task/vpcv1.2/variable.tf
@@ -17,14 +17,24 @@ variable "availability_zone" {
 }
 #variables for CIDR blocks
 
-variable "subnet_cidr_blocks" {
+variable "public_cidr_blocks" {
   type = list(string)
-  default = ["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24","10.0.11.0/24","10.0.12.0/24","10.0.13.0/24"]
-  description = "define subnets CIDR blocks"
+  default = ["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24"]
+  description = "define public subnets CIDR blocks"
 }
-variable "subnet_names" {
+variable "private_cidr_blocks" {
   type = list(string)
-  default = ["public_a","public_b","public_c","private_a","private_b","private_c"]
+  default = ["10.0.11.0/24","10.0.12.0/24","10.0.13.0/24"]
+  description = "define private subnets CIDR blocks"
+}
+variable "public_subnet_names" {
+  type = list(string)
+  default = ["public_a","public_b","public_c"]
+  description = "define subnet names"
+}
+variable "Private_subnet_names" {
+  type = list(string)
+  default = ["private_a","private_b","private_c"]
   description = "define subnet names"
 }
 variable "cidr_blocks" {
