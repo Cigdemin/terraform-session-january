72720d696f57ee4281d57f702d7644f998ef88ea
cigdemin <cigmenci@hotmail.com>
2023-02-13 11:35:29 -0600

    code fixed

--- task/vpcv1.2/data_source.tf
+++ task/vpcv1.2/data_source.tf
@@ -3,6 +3,6 @@ data "aws_subnet" "public" {
 count = length(var.subnet_names)
   filter {
     name = "tag:name"
-    values = format("["subnet_public_%s"]", element(var.subnet_names, count.index))
+    values = [format("subnet_public_%s", element(var.subnet_names, count.index))]
   }
 }
