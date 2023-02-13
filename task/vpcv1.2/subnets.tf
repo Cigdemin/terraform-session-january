599fb1ad5d412263a9bc12a09f79044869ad5f32
cigdemin <cigmenci@hotmail.com>
2023-02-13 10:27:59 -0600

    vpc and subnets code updated

--- task/vpcv1.2/subnets.tf
+++ task/vpcv1.2/subnets.tf
@@ -8,34 +8,12 @@
 
 
 resource "aws_subnet" "public_subnet_a" {
+  count = length(var.subnet_cidr_blocks)
   vpc_id     = aws_vpc.my_task_vpc.id
-  cidr_block = var.cidr_block_public_a
-  availability_zone = var.availability_zone_a
-}
-resource "aws_subnet" "public_subnet_b" {
-  vpc_id     = aws_vpc.my_task_vpc.id
-  cidr_block = var.cidr_block_public_b
-  availability_zone = var.availability_zone_b
-}
-resource "aws_subnet" "public_subnet_c" {
-  vpc_id     = aws_vpc.my_task_vpc.id
-  cidr_block = var.cidr_block_public_c
-  availability_zone = var.availability_zone_c
-
-
-}
-resource "aws_subnet" "private_subnet_a" {
-  vpc_id     = aws_vpc.my_task_vpc.id
-  cidr_block = var.cidr_block_private_a
-  availability_zone = var.availability_zone_a
-}
-resource "aws_subnet" "private_subnet_b" {
-  vpc_id     = aws_vpc.my_task_vpc.id
-  cidr_block = var.cidr_block_private_b
-  availability_zone = var.availability_zone_b
-}
-resource "aws_subnet" "private_subnet_c" {
-  vpc_id     = aws_vpc.my_task_vpc.id
-  cidr_block = var.cidr_block_private_c
-  availability_zone = var.availability_zone_c
+  cidr_block = element(var.subnet_cidr_blocks, count.index )
+  availability_zone = element(var.availability_zone, 2 )
+  tags = {
+    count = length(var.subnet_names)
+    "name" = "subnet_${element(var.subnet_names, count.index )}"
+  }
 }
