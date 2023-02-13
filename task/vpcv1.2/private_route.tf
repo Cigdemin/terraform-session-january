f244afd5b57fed28be183f238884a5b85cfab359
cigdemin <cigmenci@hotmail.com>
2023-02-13 16:04:53 -0600

    fixed

--- task/vpcv1.2/private_route.tf
+++ task/vpcv1.2/private_route.tf
@@ -16,7 +16,7 @@ resource "aws_eip" "task-eip" {
 resource "aws_nat_gateway" "task_natgw" {
   allocation_id = aws_eip.task-eip.id
   connectivity_type = "public"
-  subnet_id         = element(aws_subnet.public_subnets,0)
+  subnet_id         = element(aws_subnet.public_subnets,0).id
   depends_on = [aws_internet_gateway.task_igw]
     tags = {
     Name = "task_natgw"
