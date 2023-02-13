8150aa95147de142ffd8ed509ff0f7c5cc69334d
cigdemin <cigmenci@hotmail.com>
2023-02-13 12:03:38 -0600

    code fixed

--- task/vpcv1.2/public_route.tf
+++ task/vpcv1.2/public_route.tf
@@ -26,6 +26,6 @@ resource "aws_route_table" "public_route_table" {
 
 resource "aws_route_table_association" "public_subnets" {
   count = length(data.aws_subnet.public)
-  subnet_id      = element(data.aws_subnet.public.id, count.index)
+  subnet_id      = data.aws_subnet.public.id
   route_table_id = aws_route_table.public_route_table.id
 }
