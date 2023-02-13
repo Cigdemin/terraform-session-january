1e6bc17585461b6533a8aab8788bd9dff7de2251
cigdemin <cigmenci@hotmail.com>
2023-02-13 12:32:02 -0600

    fixed

--- task/vpcv1.2/data_source.tf
+++ task/vpcv1.2/data_source.tf
@@ -1,8 +1,9 @@
 #fetch public and private subnets
 data "aws_subnet" "public" {
+cidr_block = 
 count = length(var.subnet_names)
   filter {
     name = "tag:name"
-    values = ["subnet_public*"]
+    values = ["public"]
   }
 }
