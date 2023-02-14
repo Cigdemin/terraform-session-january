111ca58a5e329aeaf1b310117317575d9254e53d
cigdemin <cigmenci@hotmail.com>
2023-02-13 19:23:01 -0600

    switch env

--- session-6/FileStructure/backend.tf
+++ session-6/FileStructure/backend.tf
@@ -2,10 +2,12 @@ terraform{
     backend "s3" {
         bucket = "terraform-session-september-backend-cigdem"
         region = "us-west-2"
-        key = "session-6/filestructure/stage/terraform.tfstate"
+        key = "session-6/filestructure/dev/terraform.tfstate"
         dynamodb_table = "terraform-session-sep-state-lock"
     }
 }
 
 # if you want to isolate infrastructure and reuse your configuration files you must isolate backend first
 # isolation backend (terraform.tfstate) = isolating environment
+# 1 backend at the time
+# no variable in key of Backend!
