c0f9b55c7006f4c8ff89d07706d1073d7a2cc609
cigdemin <cigmenci@hotmail.com>
2023-02-13 19:13:31 -0600

    stage env added

--- session-6/FileStructure/backend.tf
+++ session-6/FileStructure/backend.tf
@@ -2,7 +2,10 @@ terraform{
     backend "s3" {
         bucket = "terraform-session-september-backend-cigdem"
         region = "us-west-2"
-        key = "session-6/filestructure/qa/terraform.tfstate"
+        key = "session-6/filestructure/stage/terraform.tfstate"
         dynamodb_table = "terraform-session-sep-state-lock"
     }
 }
+
+# if you want to isolate infrastructure and reuse your configuration files you must isolate backend first
+# isolation backend (terraform.tfstate) = isolating environment
