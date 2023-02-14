eff7c6790fc187a0515d38109502b5f4e7539b10
cigdemin <cigmenci@hotmail.com>
2023-02-13 20:57:23 -0600

    folderstructure added

---
+++ session-6/Folderstructure/qa/backend.tf
@@ -0,0 +1,13 @@
+terraform{
+    backend "s3" {
+        bucket = "terraform-session-september-backend-cigdem"
+        region = "us-west-2"
+        key = "session-6/folderstructure/qa/terraform.tfstate"
+        dynamodb_table = "terraform-session-sep-state-lock"
+    }
+}
+
+# if you want to isolate infrastructure and reuse your configuration files you must isolate backend first
+# isolation backend (terraform.tfstate) = isolating environment
+# 1 backend at the time
+# no variable in key of Backend!
