68b86dfb974ef5c5f91a1c32f3ce5397766c45fd
cigdemin <cigmenci@hotmail.com>
2023-02-16 11:32:51 -0600

    updated code

---
+++ task/vpcv1.3/backend.tf
@@ -0,0 +1,14 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-session-september-backend-cigdem"
+    region = "us-west-2"
+    key    = "tasks/terraform.tfstate"  #where does terraform need to store your file.Path or 
+    dynamodb_table = "terraform-session-sep-state-lock" #Lock table
+  }
+}
+
+#Most important rule!
+#you can not use any variables in key!
+#
+
+
