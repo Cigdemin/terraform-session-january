125bb1a9452e5730f100aab2999a6d38597bc594
cigdemin <cigmenci@hotmail.com>
2023-02-20 19:08:39 -0600

    provisioner information and index.html added

--- session-8/main.tf
+++ session-8/main.tf
@@ -3,6 +3,18 @@ resource "aws_instance" "main" {
     instance_type = "t2.micro"
     vpc_security_group_ids = [ aws_security_group.main_sg.id ]
     key_name = aws_key_pair.terraform_server.key_name
+
+    provisioner "file" {
+      source = "~/home/ec2-user/terraform-session-january/session-8/index.html"   #the path on Terraform Server
+      destination = "/tmp/index.html"   #the path for the Remote Server
+
+      connection {
+        type = "ssh"
+        user =  "ec2-user"#user on Remote system
+        host = self.public_ip  #self mean same block ip address
+        private_key = file("~/.ssh/id_rsa")
+      }
+    }
 }
 
 resource "aws_key_pair" "terraform_server" {
