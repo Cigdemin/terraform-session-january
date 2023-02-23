28c3d985f3b9dd003ff59f049b697fae5665a358
cigdemin <cigmenci@hotmail.com>
2023-02-22 19:40:05 -0600

    session-9  and child module added

---
+++ modules/ec2/variables.tf
@@ -0,0 +1,17 @@
+variable "env" {
+  type = string
+  default = "dev"
+  description = "This variable is for environment"
+}
+
+variable "ami" {
+    type = string
+    default = ""
+    description = "AMI for EC2 instance"  
+}
+
+variable "instance_type" {
+    type = string
+    default = "t2.micro"
+    description = "Instance size"   
+}
---
+++ session-9/dev/main.tf
@@ -0,0 +1,13 @@
+# Terraform modules
+# 1. Child module : where you declare resources
+    # 1.1. Local Module= module is in your filesystem
+    # 1.2.Remote Module = SCM,HTTP URL, Terraform Cloud or Enterprise Provate Registery
+# 2. Root Module : call the child module, run Terraform command
+
+module "ec2_instance" {
+  source = "../../modules/ec2" #where the child module is
+####################### Variables ###########################
+  env = "dev"
+  ami = "ami-06e85d4c3149db26a"
+  instance_type = "t2.micro"
+}
