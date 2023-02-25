677f827554f47c0c4e000ba4b43c0a9c08df74e8
cigdemin <cigmenci@hotmail.com>
2023-02-25 11:41:01 -0600

    code updated

--- session-10/main.tf
+++ session-10/main.tf
@@ -1,7 +1,7 @@
 # Calling a child module remotely
 
 module "ec2" {
-    source = "github.com/Cigdemin/terraform-session-january/tree/main/modules/ec2"
+    source = "github.com/Cigdemin/terraform-session-january/main/modules/ec2"
     env = "dev"
     ami = "ami-06e85d4c3149db26a"
     instance_type = "t2.micro"
