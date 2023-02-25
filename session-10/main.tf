77e1f6920baee8d632421563a0c74992a25324d8
cigdemin <cigmenci@hotmail.com>
2023-02-25 12:11:34 -0600

    source path fixed

--- session-10/main.tf
+++ session-10/main.tf
@@ -1,8 +1,14 @@
 # Calling a child module remotely
 
 module "ec2" {
-    source = "github.com/Cigdemin/terraform-session-january/main/modules/ec2"
+    source = "github.com/Cigdemin/terraform-session-january/modules/ec2"
     env = "dev"
     ami = "ami-06e85d4c3149db26a"
     instance_type = "t2.micro"
 }
+# github.com
+# /Cigdemin
+#/terraform-session-january
+#/tree
+#/main
+#/modules/ec2
