5a65ed297c01396e2933eb1a864e3b53b9fb55f3
cigdemin <cigmenci@hotmail.com>
2023-03-07 10:58:17 -0600

    providers, version ,sg file added

--- session-11/main.tf
+++ session-11/main.tf
@@ -3,3 +3,10 @@ resource "aws_sqs_queue" "main" {
 }
 
 # terraform.workspace = workspace name
+
+# for each, for loop in terraform
+# dynamic resource creation in Terraform
+# Terraform CLI vs Cloud vs Enterprise
+# Terraform validate, fmt, import, providers, refresh, state, taint, show
+# Environment Variable - TF_LOG, TF_LOG_PATH, TF_INPUT, TF_VAR_name, TF_CLI_ARG, 
+# functions- slice, concat, lookup, file, 
---
+++ terraformpractice/sg.tf
@@ -0,0 +1,25 @@
+variable "sg_inbound_ports" {
+  type = list(number)
+  description = "list of ingress ports"
+  default = [ 22, 80, 8200, 8201, 8300, 9200, 9500 ]
+}
+
+
+
+
+resource "aws_security_group" "dynamic_sg" {   
+  name        = "dynamic_sg"
+  description = "Allow inbound traffic with dynamic block"
+
+  dynamic "ingress" {
+    for_each = var.sg_inbound_ports
+    content {
+      from_port = ingress.value
+      to_port = ingress.value
+      protocol = "tcp"
+      cidr_blocks = [ "0.0.0.0/0" ]
+    }
+
+  }
+
+}
