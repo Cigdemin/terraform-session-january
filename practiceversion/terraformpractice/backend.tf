a98b6cb1e4ed1a2fafcd9734df120c0d8e3f33b3
cigdemin <cigmenci@hotmail.com>
2023-03-18 21:34:37 -0500

    backend updtaed

--- practiceversion/terraformpractice/backend.tf
+++ practiceversion/terraformpractice/backend.tf
@@ -3,6 +3,5 @@ terraform {
     bucket = "terraform-session-september-backend-cigdem"
     region = "us-west-2"
     key = "practiceversion/terraform.tfstate"
-    dynamodb_table = "terraform-session-sep-state-lock"
   }
 }
