0934bbe23c312be7acb4dd5fb58b0c15a1fd3600
cigdemin <cigmenci@hotmail.com>
2023-03-18 21:30:10 -0500

    version updateed for terraform practice

--- session-9/dev/main.tf
+++ session-9/dev/main.tf
@@ -1,7 +1,7 @@
 # Terraform modules
 # 1. Child module : where you declare resources
     # 1.1. Local Module= module is in your filesystem
-    # 1.2.Remote Module = SCM,HTTP URL, Terraform Cloud or Enterprise Provate Registery
+    # 1.2.Remote Module = SCM,HTTP URL, Terraform Cloud or Enterprise Private Registery
 # 2. Root Module : call the child module, run Terraform command
 
 module "ec2_instance" {
