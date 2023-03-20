b05f0d3f9abac9cffc9b3bf536c2bff3bc689ce0
cigdemin <cigmenci@hotmail.com>
2023-03-19 20:58:37 -0500

    one sg deleted

--- practiceversion/terraformpractice/sg.tf
+++ practiceversion/terraformpractice/sg.tf
@@ -21,20 +21,3 @@ resource "aws_security_group" "dynamic_sg" {
   }
 
 }
-
-resource "aws_security_group" "cigdem_sg" {   
-  name        = "cigdem_sg"
-  description = "Allow inbound traffic with dynamic block"
-
-  dynamic "ingress" {
-    for_each = var.sg_inbound_ports
-    content {
-      from_port = ingress.value
-      to_port = ingress.value
-      protocol = "tcp"
-      cidr_blocks = [ "0.0.0.0/0" ]
-    }
-
-  }
-
-}
