51d463ed0ce9e596e6aee7e8c8ac4eec0d8af365
cigdemin <cigmenci@hotmail.com>
2023-03-19 20:55:49 -0500

    new sg added

--- practiceversion/terraformpractice/sg.tf
+++ practiceversion/terraformpractice/sg.tf
@@ -5,9 +5,24 @@ variable "sg_inbound_ports" {
 }
 
 
-
-
 resource "aws_security_group" "dynamic_sg" {   
+  name        = "dynamic_sg"
+  description = "Allow inbound traffic with dynamic block"
+
+  dynamic "ingress" {
+    for_each = var.sg_inbound_ports
+    content {
+      from_port = ingress.value
+      to_port = ingress.value
+      protocol = "tcp"
+      cidr_blocks = [ "0.0.0.0/0" ]
+    }
+
+  }
+
+}
+
+resource "aws_security_group" "cigdem_sg" {   
   name        = "dynamic_sg"
   description = "Allow inbound traffic with dynamic block"
 
